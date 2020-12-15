506f3b8d6e5419875952e972df37b34d6d9482a8
sacphan <56332124+sacphan@users.noreply.github.com>
2020-12-15 16:44:03 +0700

    update

--- UserAPI/Models/ChatHub.cs
+++ UserAPI/Models/ChatHub.cs
@@ -1,13 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace UserAPI.Models
 {
     public class ChatHub : Hub
     {
+        public static List<string> userOnline = new List<string>();
         public async Task Message(MessageModel message)
         {
             await Clients.Others.SendAsync("message", message);
+        }
+        public async Task Online(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                if (!userOnline.Contains(username))
+                {
+                    userOnline.Add(username);
+                }                  
+                await Clients.All.SendAsync("online", userOnline);
+            }    
+           
+        }
+        public async Task Offline(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                if (userOnline.Contains(username))
+                {
+                    userOnline.Remove(username);
+                }
+                
+                await Clients.All.SendAsync("offline", userOnline);
+            }
         }
     }
 
